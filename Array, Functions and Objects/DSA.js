cb54a3288fd00e94ff79f34d7eff585596453c95
Sudhanshu Shukla <your-github-email@example.com>
2025-12-28 20:52:25 +0530

    DSA

--- Array, Functions and Objects/DSA.js
+++ Array, Functions and Objects/DSA.js
@@ -9,3 +9,17 @@ let reversedArray = (a)=>{
     return reversedArray;
 }
 console.log(`reversed array is: ${reversedArray(a)}`);
+
+// Que 2: find max in array
+
+const b = [1,5,3,9,2,56,89,7];
+let max = (b)=>{
+    let max=0;
+    for(let i=0; i<=b.length-1; i++){
+        if(b[i]>max){
+            max=b[i];
+        }
+    }
+    return max;
+}
+console.log(`max in array is: ${max(b)}`);
